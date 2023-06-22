fde3cc6abbef5960667394cc042c84bf96c0ca97
Eugene-dev-opps <motieugene0@gmail.com>
2023-06-21 20:55:44 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+unsigned long int fib1 = 1, fib2 = 2, fib_sum = 0, even_sum = 2;
+
+while (fib_sum <= 4000000)
+{
+fib_sum = fib1 + fib2;
+
+if (fib_sum % 2 == 0)
+even_sum += fib_sum;
+
+fib1 = fib2;
+fib2 = fib_sum;
+}
+
+printf("%lu\n", even_sum);
+
+return (0);
+}
