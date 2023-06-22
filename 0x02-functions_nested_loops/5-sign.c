73f712689ec987ef877e42a51d31f0ed52138bb2
Eugene-dev-opps <motieugene0@gmail.com>
2023-06-21 19:01:14 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main.h" 
 
 /**
  * print_sign - Prints the sign of a number.
@@ -11,16 +11,16 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-return 1;
+return (1);
 }
 else if (n == 0)
 {
 _putchar('0');
-return 0;
+return (0);
 }
 else 
 {	    
 _putchar('-');
-return -1;
+return (-1);
 }
 }
