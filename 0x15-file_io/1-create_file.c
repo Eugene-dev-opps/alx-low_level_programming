2721305951f18678049890da3dd3ce829be060c9
Eugene-dev-opps <motieugene@gmail.com>
2023-08-25 05:31:54 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,36 +4,31 @@
 #include <unistd.h>
 #include "main.h"
 
-/**
- * create_file - Creates a file and writes text content to it.
- * @filename: The name of the file to create.
- * @text_content: The content to write to the file.
- *
- * Return: 1 on success, -1 on failure.
- */
 int create_file(const char *filename, char *text_content)
 {
+int file_descriptor;
+ssize_t content_length, write_count;
+
 if (filename == NULL)
 return (-1);
 
-int file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (file_descriptor == -1)
 return (-1);
 
 if (text_content != NULL)
 {
-ssize_t content_length = 0;
+content_length = 0;
 while (text_content[content_length] != '\0')
 content_length++;
 
-ssize_t write_count = write(file_descriptor, text_content, content_length);
+write_count = write(file_descriptor, text_content, content_length);
 if (write_count != content_length)
 {
 close(file_descriptor);
 return (-1);
 }
 }
-
 close(file_descriptor);
 return (1);
 }
