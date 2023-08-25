ec132743e25f92a26fdc196757adf7249944ec6c
Eugene-dev-opps <motieugene@gmail.com>
2023-08-25 04:31:00 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,13 @@
 #include <unistd.h>
 #include "main.h"
 
-/*read_textfile - to read the file name*/
+/**
+ * read_textfile - Reads a text file and prints to the POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed, or 0 on failure.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 FILE *file;
