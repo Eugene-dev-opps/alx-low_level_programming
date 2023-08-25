a404c96269e8576eb497cf72af87f749131eb4ca
Eugene-dev-opps <motieugene@gmail.com>
2023-08-25 07:05:04 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,33 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
 
-/**
- * append_text_to_file - At the end of a file it appends text.
- * @filename: A pointer to the name of the file.
- * @text_content: The string to add to the end of the file.
- * Return: If the function fails or filename is NULL - -1.
- *         If the file does not exist the user lacks write permissions - -1.
- *         Otherwise - 1.
- */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+int file_descriptor;
+ssize_t content_length, write_count;
 
-	if (filename == NULL)
-		return (-1);
+if (filename == NULL)
+return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+file_descriptor = open(filename, O_WRONLY | O_APPEND);
+if (file_descriptor == -1)
+return (-1);
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+if (text_content != NULL)
+{
+content_length = 0;
+while (text_content[content_length] != '\0')
+content_length++;
 
-	if (o == -1 || w == -1)
-		return (-1);
+write_count = write(file_descriptor, text_content, content_length);
+if (write_count != content_length)
+{
+close(file_descriptor);
+return (-1);
+}
+}
 
-	close(o);
-
-	return (1);
+close(file_descriptor);
+return (1);
 }
