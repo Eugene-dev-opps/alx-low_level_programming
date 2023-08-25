f61dd0a7400418e57df2dd1d2bb592896940b337
Eugene-dev-opps <motieugene@gmail.com>
2023-08-25 05:19:44 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the file name to create.
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * create_file - Creates a file and writes text content to it.
+ * @filename: The name of the file to create.
+ * @text_content: The content to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+if (filename == NULL)
+return (-1);
 
-	if (filename == NULL)
-		return (-1);
+int file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (file_descriptor == -1)
+return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+if (text_content != NULL)
+{
+ssize_t content_length = 0;
+while (text_content[content_length] != '\0')
+content_length++;
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+ssize_t write_count = write(file_descriptor, text_content, content_length);
+if (write_count != content_length)
+{
+close(file_descriptor);
+return (-1);
+}
+}
 
-	if (fd == -1 || w == -1)
-		return (-1);
-
-	close(fd);
-
-	return (1);
+close(file_descriptor);
+return (1);
 }
