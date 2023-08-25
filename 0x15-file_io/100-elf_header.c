8e361e6004f77322e5c74e850312af941751d8b6
Eugene-dev-opps <motieugene@gmail.com>
2023-08-25 08:51:00 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -37,38 +37,28 @@ switch (elf_header.e_ident[EI_CLASS])
 {
 case ELFCLASS32:
 printf(" ELF32\n");
-break;
 case ELFCLASS64:
 printf(" ELF64\n");
-break;
 default:
 printf(" <unknown>\n");
-break;
 }
-
-printf("  Data:");
+printf("Data:");
 switch (elf_header.e_ident[EI_DATA])
 {
 case ELFDATA2LSB:
 printf(" 2's complement, little endian\n");
-break;
 case ELFDATA2MSB:
 printf(" 2's complement, big endian\n");
-break;
 default:
 printf(" <unknown>\n");
-break;
 }
-
 printf("  Version: %d\n", elf_header.e_ident[EI_VERSION]);
 printf("  OS/ABI: %d\n", elf_header.e_ident[EI_OSABI]);
 printf("  ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
 printf("  Type: %d\n", elf_header.e_type);
 printf("  Entry point address: 0x%x\n", elf_header.e_entry);
-
 close(fd);
 }
-
 int main(int argc, char *argv[])
 {
 if (argc != 2)
