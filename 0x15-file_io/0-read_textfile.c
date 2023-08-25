af9ec414e8796a5ef543ac99315a008c30ba9a6e
Eugene-dev-opps <motieugene@gmail.com>
2023-08-25 04:24:25 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,31 +3,35 @@
 #include <unistd.h>
 #include "main.h"
 
+/*read_textfile - to read the file name*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+FILE *file;
+char *buffer;
+ssize_t read_count, write_count;
+
 if (filename == NULL)
 return (0);
 
-FILE *file = fopen(filename, "r");
+file = fopen(filename, "r");
 if (file == NULL)
 return (0);
 
-char *buffer = malloc(sizeof(char) * (letters + 1));
+buffer = malloc(sizeof(char) * (letters + 1));
 if (buffer == NULL)
 {
 fclose(file);
 return (0);
 }
 
-ssize_t read_count = fread(buffer, sizeof(char), letters, file);
+read_count = fread(buffer, sizeof(char), letters, file);
 if (read_count <= 0)
 {
 fclose(file);
 free(buffer);
 return (0);
 }
-
-ssize_t write_count = write(STDOUT_FILENO, buffer, read_count);
+write_count = write(STDOUT_FILENO, buffer, read_count);
 if (write_count < read_count)
 {
 fclose(file);
